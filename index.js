2809385edc3524f7eff00dd5d1d12dfa47079792
Umpoy <iumpoy@yahoo.com>
2018-10-20 10:50:23 -0700

    converted to ES6

--- index.js
+++ index.js
@@ -2,25 +2,31 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-app.get('/', function (req, res) {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function (socket) {
-    socket.on('chat message', function (msg) {
+io.on('connection', socket => {
+    var name;
+    socket.on('chat message', msg => {
         io.emit('chat message', msg);
     });
-    socket.on('connected', function (person) {
+    socket.on('connected', person => {
+        name = String(person)
         io.emit('connected', person);
     });
-    socket.on('disconnect', function (person) {
-        io.emit('disconnect', person);
+    socket.on('disconnect', () => {
+        io.emit('disconnect');
+    });
+    socket.on('typing', () => {
+        let hello = 'hello';
+        io.emit('typing', hello);
+        sleep()
     })
 
 });
 
 
-
-http.listen(3000, function () {
+http.listen(3000, () => {
     console.log('listening on *:3000');
 });
