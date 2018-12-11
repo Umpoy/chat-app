5a5ed250df3b1575d39f5e74b2148acb8e57706c
Umpoy <iumpoy@yahoo.com>
2018-12-11 09:52:20 -0800

    added socket to client side

--- index.js
+++ index.js
@@ -11,25 +11,32 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', socket => {
-    socket.on('chat message', msg => {
-        io.emit('chat message', msg);
-    });
+    // socket.on('chat message', msg => {
+    //     io.emit('chat message', msg);
+    // });
 
     socket.on('connected', name => {
-        io.emit('connected', name);
-        console.log('connect')
-        users[socket.id] = { name }
-        users[socket.id].words = wordsArray.splice(0, 3);
-        console.log(wordsArray.length)
-        console.log(users)
-        io.emit('grabUsers', users);
+        // io.emit('connected', name);
+        // console.log('connect')
+        // users[socket.id] = { name }
+        // users[socket.id].words = wordsArray.splice(0, 3);
+        // console.log(wordsArray.length)
+        // console.log(users)
+        // io.emit('grabUsers', users);
+        console.log('connected with react 😉');
+        name.on('subscribeToTimer', (interval) => {
+            console.log('client is subscribing to timer with interval ', interval);
+            setInterval(() => {
+                name.emit('timer', new Date());
+            }, interval);
+        });
     });
 
 
     socket.on('disconnect', () => {
-        io.emit('disconnect', users[socket.id]);
-        delete users[socket.id]
-        console.log(users);
+        // io.emit('disconnect', users[socket.id]);
+        // delete users[socket.id]
+        // console.log(users);
     });
     // socket.on('typing', () => {
     //     let hello = 'hello';
